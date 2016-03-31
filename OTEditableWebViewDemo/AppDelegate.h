90ca7215d87ad51e952dc66308d2432ca01bc20a
OpenThread <openfibers@gmail.com>
2016-03-31 17:57:35 +0800

    Initial Commit

---
+++ OTEditableWebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OTEditableWebViewDemo
+//
+//  Created by openthread on 3/31/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
